40c47734d36a74ae109854bf2fd606a4627ea65e
scythe <scythe@hotbox.ru>
2011-05-02 18:04:56 +0000

    added most of game logic

--- whist.pl
+++ whist.pl
@@ -18,9 +18,13 @@ $VERSION = '0.99.3';
 # global variables
 our @hands;	# Contains cards in the order of shuffling, so that player 1's hand is cards 0-12, player 2 has cards 13-25, et cetera. 
 our @deck;	# Acts as a reverse-index to the hands array.
-our $playing;
+our $playing;	# 0 -- not playing, 1 -- playing, 2 -- adding players
 our $trick;	# number of the trick we're on. Counts from 1, I know, sue me.
 our @players;
+our $numplayers;
+our $turn;
+our @trickcards;
+our @player_scores;
 
 sub array_concat {
 	my $ret = "";
@@ -32,8 +36,8 @@ sub array_concat {
 
 sub new_deck {		# defines a bunch of global variables. Sue me, SPJ.
 
-	$playing = 1;
-	$trick = 1;		
+	$playing = 2;
+	$trick = 0;		
 	@deck = ();		
 	@hands = ();	
 	
@@ -70,9 +74,52 @@ sub play_card {
 		$server->command("MSG $target That's not your card!");
 		return;
 	}
+	$trickcards[$turn] = $hands[$position];
 	$hands[$position] = -1;
 	$deck[$cardnum] = -1;
-	return;
+	$turn++;
+	if($turn == 4) {
+		return score_trick($server, $target);
+	}
+}
+
+sub score_trick {
+	my ($server, $target) = @_;
+	my $tc_max = -1;
+	my $tc_winner = -1;
+	for(my $i = 0; $i < 4; $i++) {
+		if($trickcards[$i] > $tc_max) {
+			$tc_winner = $i;
+			$tc_max = $trickcards[$i];
+		}
+	}
+	$server->command("MSG $target " . $players[$tc_winner] . " wins the trick with " . untranslate($tc_max));
+	$player_scores[$tc_winner] += 1;
+	$server->command("MSG $target " . array_concat(@players));
+	$server->command("MSG $target " . array_concat(@player_scores));
+	$turn = 0;
+	
+	if($trick == 13) {
+		return end_game($server, $target);
+	}
+}
+
+sub end_game {
+	my ($server, $target) = @_;
+	my $ps_winner = -1;
+	my $ps_max = -1;
+	for(my $i = 0; $i < 4; $i++) {
+		if($player_scores[$i] > $ps_max) {
+			$ps_winner = $i;
+			$ps_max = $player_scores[$i];
+		}
+	}
+	$server->command("MSG $target " . $players[$ps_winner] . " wins with " . $player_scores[$ps_winner] . " points!");
+	$playing = 0;
+	$trick = 0;
+	$numplayers = 0;
+	$turn = 0;
+	@player_scores = qw(0 0 0 0);
 }
 
 sub handle_msgs {
@@ -81,27 +128,25 @@ sub handle_msgs {
 
 	my $serv = Irssi::active_server();
 
-	if(substr($message,0,5) eq "!deal" and length($message) > 6) {
-		
-		if($playing == 0) {
-			
-			@players = split(/\s+/, substr($message,6));
-			Irssi::print(array_concat(@players));
-			
-			my $last = $players[3] or Irssi::print("Not enough players!");
-			if($last) {
-				new_deck();
-			}
-		} else {
-			$server->command("MSG $target We're already playing!");
-		}
+	if(substr($message,0,5) eq "!deal" and ($playing == 0 or $server->command("MSG $target We're already playing!") and 0)) {
+		new_deck();
 	}
-	Irssi::print(substr($message, 0, 5));
-	if(substr($message,0,5) eq "!play") {
-		if($playing == 1) {
-			play_card(translate(substr($message, 6)), substr($nick,0,length($nick) - 1), $server, $target);
-		} else {
-			$server->command("MSG $target We haven't started yet!");
+	if(substr($message,0,5) eq "!play" and ($playing == 1 or $server->command("MSG $target We haven't started yet!") and 0)) {
+		play_card(translate(substr($message, 6)), substr($nick,0,length($nick) - 1), $server, $target);
+	}
+	if(substr($message, 0, 5) eq "!join" and ($playing == 2 or $server->command("MSG $target You have to wait for the next game.") and 0)) {
+		$players[$numplayers] = $nick;
+		$numplayers++;
+		my $hand = "";
+		for(my $i = ($numplayers - 1) * 13; $i < $numplayers * 13; $i++ ) {
+			$hand = $hand . untranslate($hands[$i]);
+		}
+		$server->command("MSG $nick Your hand contains $hand ");
+		if($numplayers = 4) {
+			$trick = 1;
+			$turn = 0;
+			@player_scores = qw(0 0 0 0);
+			$server->command("MSG " . $players[0] . " it's your turn.");
 		}
 	}
 	#Irssi::print("server: $server data: $data nick: $nick address: $address");
